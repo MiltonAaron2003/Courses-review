e62533c418b8de776dcea3f7ae5aed0ad4b39cfa
MiltonAaron2003 <miltonaaron3@gmail.com>
2025-11-09 10:43:05 -0600

    🚩Definir relaciones Eloquent en modelos User, Course y Review

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    public function reviews() {
+    return $this->hasMany(Review::class);
+}
+
+// Esto es para usar el 'slug' en la URL en lugar del ID
+public function getRouteKeyName() {
+    return 'slug';
+}
 }
