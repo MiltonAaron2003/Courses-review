1caa8b65832afdff3a00b4a9a93feaf1fc7d1052
MiltonAaron2003 <miltonaaron3@gmail.com>
2025-11-09 10:34:48 -0600

    🚩Creacion de migraciones de Course y Review

---
+++ database/migrations/2025_11_09_163221_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('reviews', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Conecta con el usuario
+        $table->foreignId('course_id')->constrained()->onDelete('cascade'); // Conecta con el curso
+        $table->unsignedTinyInteger('rating'); // Calificación del 1 al 5
+        $table->text('comment');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
